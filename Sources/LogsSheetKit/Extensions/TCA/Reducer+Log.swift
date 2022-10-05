31f65bfe73d03a7f4b71b063f0b3c66c95eade09
yermukhanbet <y.yesen.y@gmail.com>
2022-10-05 18:01:30 +0900

    :sparkles: Add LoggableState

--- Sources/LogsSheetKit/Extensions/TCA/Reducer+Log.swift
+++ Sources/LogsSheetKit/Extensions/TCA/Reducer+Log.swift
@@ -3,6 +3,7 @@
 //  
 //
 //  Created by Yessen Yermukhanbet on 2022/10/05.
+//  Copyright © 2022 Riiid Inc. All rights reserved.
 //
 
 import Foundation
@@ -17,8 +18,4 @@ extension Reducer where State: LoggableState {
       return self.run(&state, action, environment)
     }
   }
-}
-
-protocol LoggableState: Equatable {
-  var logs: [ActionLog] { get set }
 }
---
+++ Sources/LogsSheetKit/LoggableState.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yessen Yermukhanbet on 2022/10/05.
+//  Copyright © 2022 Riiid Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol LoggableState: Equatable {
+  var logs: [ActionLog] { get set }
+}
+
+extension LoggableState {
+  var logs: [ActionLog] {
+    get {
+      return GlobalLogs.shared.logs
+    }
+    set {
+        GlobalLogs.shared.logs = newValue
+    }
+  }
+}
+
+struct GlobalLogs {
+  static var shared: GlobalLogs = GlobalLogs()
+  var logs: [ActionLog] = []
+}
