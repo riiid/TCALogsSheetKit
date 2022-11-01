04993acbccfd8fdfae409573cff53e8469cc6fa8
Yessen <y.yesen.y@gmail.com>
2022-11-01 18:43:50 +0900

    :recycle: refactor to ReducerProtocol

--- Sources/TCALogsSheetKit/TCALogger.swift
+++ Sources/TCALogsSheetKit/TCALogger.swift
@@ -39,3 +39,33 @@ extension AnyReducer {
     }
   }
 }
+
+extension ReducerProtocol {
+  /// Saves each action  run through the reducer as the log message if debug build configuration is on
+  /// - Parameters:
+  ///   - isDebug: is debug configuration on
+  public func log(isDebug: Bool) -> some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      if isDebug {
+        LogsSheetManager.shared.log(message: "\(action)")
+      }
+      return self.reduce(into: &state, action: action)
+    }
+  }
+
+  /// Saves the custom log message if debug build configuration is on
+  /// - Parameters:
+  ///   - isDebug: is debug configuration on
+  ///   - message: custom log message
+  public func log(
+    isDebug: Bool,
+    with message: @escaping ((Action) -> String)
+  ) -> some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      if isDebug {
+        LogsSheetManager.shared.log(message: message(action))
+      }
+      return self.reduce(into: &state, action: action)
+    }
+  }
+}
