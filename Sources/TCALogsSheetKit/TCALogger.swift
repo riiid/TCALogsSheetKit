fa50327ea6dac69099fbed6ed9bcc470e2b1ac4e
Yessen <y.yesen.y@gmail.com>
2023-08-21 14:20:02 +0900

    🔥 Remove ANyReducer loger

--- Sources/TCALogsSheetKit/TCALogger.swift
+++ Sources/TCALogsSheetKit/TCALogger.swift
@@ -10,35 +10,9 @@ import Foundation
 import ComposableArchitecture
 import LogsSheetKit
 
-extension AnyReducer {
   /// Saves each action  run through the reducer as the log message if debug build configuration is on
   /// - Parameters:
   ///   - isDebug: is debug configuration on
-  public func log(isDebug: Bool) -> Self {
-    AnyReducer.init { state, action, environment in
-      if isDebug {
-        LogsSheetManager.shared.log(message: "\(action)")
-      }
-      return self.run(&state, action, environment)
-    }
-  }
-
-  /// Saves the custom log message if debug build configuration is on
-  /// - Parameters:
-  ///   - isDebug: is debug configuration on
-  ///   - message: custom log message
-  public func log(
-    isDebug: Bool,
-    with message: @escaping ((Action) -> String)
-  ) -> Self {
-    AnyReducer.init { state, action, environment in
-      if isDebug {
-        LogsSheetManager.shared.log(message: message(action))
-      }
-      return self.run(&state, action, environment)
-    }
-  }
-}
 
 extension ReducerProtocol {
   /// Saves each action  run through the reducer as the log message if debug build configuration is on
